c6a16f18755b6c03623e8aedc34420a5e3db1d09
davidjvalladares2493 <80606069+davidjvalladares2493@users.noreply.github.com>
2022-03-14 10:37:56 -0600

    Estilos de los elementos de lista
    Se cambia el estilo de los elementos de la lista

--- src/components/task.js
+++ src/components/task.js
@@ -8,7 +8,18 @@ class Task extends Component {
     return{
       fontSize: '20px',
       color: this.props.task.done ? 'green' : 'red',
-      textDecoration: this.props.task.done ? 'line-through' : 'none'
+      textDecoration: this.props.task.done ? 'line-through' : 'none',
+      padding: '20px',
+      border: '1px solid #F0F8FF',
+      borderRadius: '15px',
+      width: 'max-content',
+      marginBottom: '40px',
+      marginTop: '10px',
+      padding: '10px 15px',
+      border:'none',
+      fontSize: '14px',
+      borderRadius: '5px'
+
     }
   }
 
@@ -16,10 +27,10 @@ class Task extends Component {
     const {task} = this.props;
     //const redColor = {background: 'green'}; //style={redColor}
     return <p style={this.StyleCompleted()}>
+              {task.id} -
               {task.title} -
               {task.description} -
-              {task.done} -
-              {task.id}
+              {task.done}
               <input type="checkbox" onChange = {this.props.checkDone.bind(this, task.id)}/>
               <button style={btnDelete} onClick={this.props.deleteTask.bind(this, task.id)}>
               x
@@ -37,7 +48,7 @@ const btnDelete = {
   background: '#ea2027',
   color: '#fff',
   border: 'none',
-  padding: '10px 15px',
+  padding: '5px 10px',
   borderRadius: '50%',
   cursor: 'pointer',
   float: 'right'
